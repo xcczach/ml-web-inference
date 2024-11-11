4d9631503ab76bd7bdc3749d7247b495d173c045
xcc <2867389537@qq.com>
2024-11-11 20:34:54 +0800

    expose function complete

--- ml_web_inference/__init__.py
+++ ml_web_inference/__init__.py
@@ -2,4 +2,11 @@ from .main import expose
 from .main import Request, Response
 from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
 
-__all__ = ["expose", "Request", "Response", "StreamingResponse"]
+__all__ = [
+    "expose",
+    "Request",
+    "Response",
+    "StreamingResponse",
+    "FileResponse",
+    "JSONResponse",
+]
